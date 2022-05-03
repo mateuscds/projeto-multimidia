d3f241fadcc919c3faa323cb8450d24a773405f4
biancasarabia <biols@cin.ufpe.br>
2022-05-03 19:05:42 -0300

    organizing code

--- teste.py
+++ teste.py
@@ -1,114 +1,105 @@
+##### IMPORTS #####
+
 import moviepy.editor as mpy
-
-from pydub import AudioSegment # to read mp3s 
-import librosa # audio analysis 
-
-from sys import argv
+from pydub import AudioSegment
+import librosa
 import numpy as np
-from time import sleep, time
+from scipy.stats import uniform
 
-from scipy.stats import lognorm, uniform
+##### GLOBAL DEFINITIONS #####
 
 vcodec =   "libx264"
 videoquality = "24"
-
-# slow, ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow
 compression = "slow"
+min_interval_between_beats = 3 #minimal time interval between beats (seconds)
 
-# PARAMETROS
+##### PARAMETERS #####
 
-title = "NEUROLAKE"
-loadtitle = title + '.mp4'
-savetitle = title + '_EDITADO' + '.mp4'
+input_music_path = 'music.mp3'
+input_video_paths = ['exemple_1.mp4', 'exemple_2.mp4', 'exemple_3.mp4']
+output_video_path = 'output.mp4'
 
 # modify these start and end times for your subclips
-cuts = [('00:00:02.949', '00:00:20.152'),
-        ('00:00:30.328', '00:00:40.077')]
+'''cuts = [('00:00:02.949', '00:00:20.152'),
+        ('00:00:30.328', '00:00:40.077')]'''
 
-# Mínimo de tempo entre as batidas
+##### FUNCTIONS #####
 
-min_time_beat = 3 #segundos
-
-########################### FUNÇÕES DE TRATAMENTO DAS BATIDAS ###########################
-
-
-def read_mp3(filename):
+def read_mp3(music_path):
+    
     """MP3 to numpy array"""
-    a = AudioSegment.from_mp3(filename)
-    y = np.array(a.get_array_of_samples(), dtype=np.float32) / 2**15
+    
+    a = AudioSegment.from_mp3(music_path)
+    y = np.array(a.get_array_of_samples(), dtype = np.float32) / 2**15
     
     if a.channels == 2:
         y = y.reshape((-1, 2))
-        y = y.mean(axis=1) # stripping to mono
+        y = y.mean(axis = 1)
 
-    # reducing sample rate (div by 2)
     y = y[::2]
     sr = a.frame_rate//2
       
     return y, sr
 
-   
-def read_wav(filename):
-    """WAV to numpy array"""
-    y, sr = librosa.load(filename)
-    return y, sr
-
-def read_any(filename):
-    """WAV or MP3 to numpy array"""
-    if filename.endswith('.mp3'):
-        return read_mp3(filename)
-    elif filename.endswith('.wav'):
-        return read_wav(filename)
-    else:
-        print("Unknown file extension")
-        # exception or sth
-        return 
+def get_beat_times(music_path):
     
-def mp3_to_wav(src, dst=None):
-    """Exports mp3 file to wav"""
-    dst = dst or src.replace('.mp3', '.wav')
-    audio = AudioSegment.from_mp3(src)
-    audio.export(dst, format="wav")
+    """Detects beat times from music_path"""
+    
+    y, sr = read_mp3(music_path)
 
+    onset_env = librosa.onset.onset_strength(y, sr = sr)
+    prior = uniform(30, 300)
+    utempo = librosa.beat.tempo(onset_envelope = onset_env, sr = sr, prior = prior)[0]
 
-def get_beat_times(filename=None, y=None, sr=None):
-    """Detects beat times from filename or np.arrray"""
-    if filename is not None:
-        y, sr = read_any(filename)
-    if y is not None and sr is not None:
-        pass # y, sr already loaded
-    else:
-        print("Something wrong with the arguments")
-        return
+    _, beat_times = librosa.beat.beat_track(y = y, sr = sr, units = 'time', bpm = utempo)
 
-    onset_env = librosa.onset.onset_strength(y, sr=sr)
-    prior = uniform(30, 300)  # uniform over 30-300 BPM
-    utempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr, prior=prior)[0]
+    return beat_times
 
-    _, beat_times = librosa.beat.beat_track(y=y, sr=sr, units='time', bpm=utempo)
+def standardize_beat_times(beat_times, min_interval_between_beats):
+    
+    """Shorttens the intervals between beats"""
 
-    return beat_times, utempo
+    new_beat_times = []
+    previous_beat = 0
 
+    for beat in beat_times:
+        if (beat - previous_beat) >= min_interval_between_beats:
+            new_beat_times.append(beat) 
+            previous_beat = beat
 
-########################### FUNÇÕES DE TRATAMENTO DOS VIDEOS ###########################
+    return new_beat_times
 
+def cut_and_mute_video(video_object, time_limits):
 
-def cut_and_mute_video(video, limits):
+    """Cuts and muttes a video"""
 
-    # cut file
-    clip = video.subclip(limits[0], limits[1])
-
-    muted_clip = clip.without_audio()
-
+    clip = video_object.subclip(time_limits[0], time_limits[1]) #cutting video
+    muted_clip = clip.without_audio() #mutting video
     return muted_clip
 
+def add_audio_to_video(video, audio):
 
-def audio_into_clip(video, audio):
+    """Adds audio to video clip"""
 
-    # adding audio to the video clip
     videoclip = video.set_audio(audio)
     return videoclip
 
+'''
+def convert_into_time_cuts(new_beat_times):
+
+    # Formato tem que ser 
+    # cuts = [('00:00:02.949', '00:00:20.152'), ('00:00:30.328', '00:00:40.077')]
+
+    clip_cuts = []
+    begin_beat = 0
+
+    for beat in new_beat_times: # tem que ser em pares
+
+            
+        clip_cuts.append(cut)
+        
+
+    return clip_cuts
 
 def edit_video(loadtitle, savetitle, cuts):
     
@@ -129,7 +120,7 @@ def edit_video(loadtitle, savetitle, cuts):
     # putting music into cutted video
 
     music = mpy.AudioFileClip("music.mp3") 
-    final_clip = audio_into_clip(final_clip, music)
+    final_clip = add_audio_to_video(final_clip, music)
 
     # save file
     final_clip.write_videofile(savetitle, threads=4, fps=24,
@@ -138,49 +129,26 @@ def edit_video(loadtitle, savetitle, cuts):
                                ffmpeg_params=["-crf",videoquality])
 
     video.close()
-
-
-def standard_beat_times(beat_times, min_time_beat):
-
-    new_beat_times = []
-    previous_beat = 0
-
-    for beat in beat_times:
-
-        if beat - previous_beat >= min_time_beat:
-            new_beat_times.append(beat) 
-            previous_beat = beat
-
-
-    return new_beat_times
-
-
-def convert_into_time_cuts(new_beat_times):
-
-    # Formato tem que ser 
-    # cuts = [('00:00:02.949', '00:00:20.152'), ('00:00:30.328', '00:00:40.077')]
-
-    clip_cuts = []
-    begin_beat = 0
-
-    for beat in new_beat_times: # tem que ser em pares
-
-            
-        clip_cuts.append(cut)
-        
-
-    return clip_cuts
-
+'''
 
 if __name__ == '__main__':
 
-    beat_times, _ = get_beat_times('music.mp3')
-    new_beat_times = standard_beat_times(beat_times, min_time_beat) #lista atualizada com o minimo de tempo que a batida tem que ter
+    beat_times = get_beat_times(input_music_path)
+    new_beat_times = standardize_beat_times(beat_times, min_interval_between_beats)
 
     print(new_beat_times)
 
-    # Agora, temos que converter para o formato de cuts
+    #a partir dos beat times, deveremos descobrir os intervalos entre cada um dos beats, para saber o comprimento de pedaços de vídeos que precisamos
 
-    clip_cuts = convert_into_time_cuts(new_beat_times)
+    #com esses intervalos em mão, para cada vídeo de entrada iremos definir limites de tempo que representem uma parte do vídeo com um dos comprimentos (podemos seguir a ordem -> primeiro vídeo = primeiro intervalo)
+    #esse pedaço pode começar do ínicio, meio, aleatório ou fim do vídeo
 
-    edit_video(loadtitle, savetitle, cuts)
+    #depois que tivermos esses limites, iremos realizar o corte de cada vídeo
+
+    #depois com todos os vídeos cortados, iremos juntar todos
+
+    #por fim, adicionaremos a música e salvaremos
+
+    #clip_cuts = convert_into_time_cuts(new_beat_times)
+
+    #edit_video(loadtitle, savetitle, cuts)
